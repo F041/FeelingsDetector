47a511090a94cc4fc03e8987d0fadda0ca7d3958
Gabriele Giordano <contact.ggiordano@gmail.com>
2016-08-23 13:49:24 +0200

    Update FeelingsDetectorOnlyGSR.py

--- FeelingsDetectorOnlyGSR.py
+++ FeelingsDetectorOnlyGSR.py
@@ -6,12 +6,14 @@ Created on Mon Aug 22 16:13:11 2016
 """
 
 from __future__ import division
-from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage.filters import gaussian_filter  
+import time
+start = time.clock()
 
-import numpy as np
-import matplotlib.pyplot as plt
 
 def GSRdata(fileName,dGSR="n",GSRs="n"):
+    import numpy as np
+    import matplotlib.pyplot as plt
     GSRTdata=np.genfromtxt(fileName,skip_header=0,delimiter=',',dtype=float, deletechars='\n')
     media_GSR=0
     GSR=[]
@@ -35,18 +37,27 @@ def GSRdata(fileName,dGSR="n",GSRs="n"):
            dGSR.append(diff)
            i+=1
         dGSR=gaussian_filter(dGSR,sigma=4)   
+        plt.xlabel("time (s)")
+        plt.ylabel("GSR difference")
+        plt.title("First forward difference")
         plt.plot(dGSR)
     if GSRs!="n":
         GSRmax=max(GSR)
+        time=range(len(GSR))
         for value in GSR:
             s_value=value/GSRmax
             GSRs.append(s_value)
         plt.show()
-        plt.plot(GSRs)
+        plt.scatter(time,GSRs)
+        plt.title("Standardized GSR")   
+        plt.xlabel("time (s)")
+        plt.ylabel("GSRs")
+
     GSR=gaussian_filter(GSR,sigma=5)
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
     ax1.plot(GSR,label='GSR')   
-    plt.legend(loc='upper right');
-    
+    plt.legend(loc='upper right')
+    plt.ylabel("GSR")
+    plt.xlabel("time (s)")
     return GSR
